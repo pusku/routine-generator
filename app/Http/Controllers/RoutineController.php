c57eaa9620e149e66fd4458cf03d43f60f55e9b2
pusku <puskuuuu@gmail.com>
2018-12-09 03:53:41 +0600

    fixed

--- app/Http/Controllers/RoutineController.php
+++ app/Http/Controllers/RoutineController.php
@@ -255,7 +255,7 @@ class RoutineController extends Controller{
                                         $aaa = Slot::select('id')->orderBy('id', 'desc')->first();
                                         $bbb = Day::select('id')->orderBy('id', 'desc')->first();
                                         while(!empty($isThereAlreadyAclass)){
-                                            if($slotId>=$aaa->id){
+                                            if($slotId<$aaa->id){
                                                 $slotId++;
                                             }else{
                                                 if($dayId+1>= $bbb->id){
@@ -273,7 +273,7 @@ class RoutineController extends Controller{
                                         $aaa = Slot::select('id')->orderBy('id', 'desc')->first();
                                         $bbb = Day::select('id')->orderBy('id', 'desc')->first();
                                         while(!empty($isThereAlreadyAclass)){
-                                            if($slotId>=$aaa->id){
+                                            if($slotId<$aaa->id){
                                                 $slotId++;
                                             }else{
 
